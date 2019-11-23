260895cf98d733ab9da007576fa495276d621eb2
zhangle <zhangle@cmss.chinamobile.com>
2019-11-23 23:47:09 +0800

    添加回文数解题答案

---
+++ LinkedList/src/OneWayLinkedList.java
@@ -0,0 +1,18 @@
+/**
+* @Description:    单向链表
+* @Author:         kirago
+* @CreateDate:     2019/10/10 11:44 AM
+* @UpdateRemark:   修改内容
+* @Version:        1.0
+*/
+public class OneWayLinkedList {
+
+    public int index;
+
+    public OneWayLinkedList next;
+
+    public OneWayLinkedList(int index) {
+        this.index = index;
+        this.next = null;
+    }
+}
