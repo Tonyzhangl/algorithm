58e76e6dd02a5613c694f4d3e6097bb3f21acbba
zhangle <zhangle@cmss.chinamobile.com>
2019-12-19 20:54:45 +0800

    爬楼梯

--- java/src/LeetCode/LC70.java
+++ java/src/LeetCode/LC70.java
@@ -11,13 +11,12 @@ public class LC70 {
 
     public static int climbStairs(int n) {
         int[] dp = new int[n+1];
-        if( n<=3) {
+        if( n<3) {
             return n;
         }
         dp[1] = 1;
         dp[2] = 2;
-        dp[3] = 3;
-        for(int i=4;i<=n;i++) {
+        for(int i=3;i<=n;i++) {
             dp[i] = dp[i-1] + dp[i-2] ;
         }
         return dp[n];
