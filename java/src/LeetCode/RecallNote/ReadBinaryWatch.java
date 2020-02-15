5fdca0706d5b0dfaae15ee77c4469c0171b1aef8
zhangle <zhangle@cmss.chinamobile.com>
2020-02-15 15:26:15 +0800

    二进制手表【利用回溯算法进行实现】

--- java/src/LeetCode/RecallNote/ReadBinaryWatch.java
+++ java/src/LeetCode/RecallNote/ReadBinaryWatch.java
@@ -11,18 +11,44 @@ import java.util.List;
 * @Version:        1.0
 */
 public class ReadBinaryWatch {
+    List<String> ans = new ArrayList<>();
 
     public List<String> readBinaryWatch(int num) {
-        List<String> times = new ArrayList<>();
 
         for(int i=0;i<12;i++){
             for(int j=0;j<60;j++){
                 // 此处时位向左移动6位表示小时位，通过或运算得到分钟位置的1的个数
                 if(Integer.bitCount(i << 6 | j) == num){
-                    times.add(String.format("%d:%02d",i,j));
+                    ans.add(String.format("%d:%02d",i,j));
                 }
             }
         }
-        return times;
+        return ans;
     }
+
+    public List<String> readBinaryWatch2(int num){
+        int[] stat = new int[10];
+        sub(num, 0, 0, stat);
+        return ans;
+    }
+
+    private void sub(int num,int start, int cnt, int[] stat){
+        if(cnt == num){
+            int hour = stat[0]*8+stat[1]*4+stat[2]*2+stat[3];
+            int minu = stat[4]*32+stat[5]*16+stat[6]*8+stat[7]*4+stat[8]*2+stat[9];
+            if(hour<12 && minu<60){
+                String s = String.format("%d:%02d", hour, minu);
+                ans.add(s);
+            }
+            return;
+        }
+
+        for(int i=start;i<=(9-(num-cnt)+1);i++){
+            stat[i] = 1;
+            sub(num, i+1, cnt+1, stat);
+            stat[i] = 0;
+        }
+        return;
+    }
+
 }
---
+++ java/src/LeetCode/RecallNote/ReadBinaryWatchTest.java
@@ -0,0 +1,9 @@
+package LeetCode.RecallNote;
+
+public class ReadBinaryWatchTest {
+
+    public static void main(String[] args){
+        ReadBinaryWatch readBinaryWatch = new ReadBinaryWatch();
+        readBinaryWatch.readBinaryWatch2(2);
+    }
+}
