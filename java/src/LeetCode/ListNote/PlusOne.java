ffbb37ab8bc420ff964d868e0169ddb7d6f07bdc
zhangle <zhangle@cmss.chinamobile.com>
2020-02-28 18:02:13 +0800

    加一

--- java/src/LeetCode/ListNote/PlusOne.java
+++ java/src/LeetCode/ListNote/PlusOne.java
@@ -37,4 +37,15 @@ public class PlusOne {
         return res;
 
     }
+
+    public int[] plusOne1(int[] digits){
+        for(int i=digits.length;i>=0;i++){
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if(digits[i] != 0) return digits;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return  digits;
+    }
 }
