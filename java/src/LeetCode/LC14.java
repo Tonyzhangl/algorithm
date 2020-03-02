eb62de7aa28e662712fb9d0c776dd57eaa0273dc
zhangle <zhangle@cmss.chinamobile.com>
2020-03-02 20:49:41 +0800

    借鉴优秀的题解

--- java/src/LeetCode/LC14.java
+++ java/src/LeetCode/LC14.java
@@ -36,6 +36,18 @@ public class LC14 {
 
     }
 
+    public static String longestCommonPrefix1(String[] strs){
+        if(strs.length == 0) return "";
+        String prefix = strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
     public static void main(String[] args) {
         String[] strings = new String[]{"flow","flow","flow"};
         System.out.println(longestCommonPrefix(strings));
