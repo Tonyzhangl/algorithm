b1faea7eda14bcbd19d6e2a95b8bd3e27d7a7922
zhangle <zhangle@cmss.chinamobile.com>
2020-03-24 18:04:15 +0800

    搜索旋转一维数组的索引

--- java/src/LeetCode/ListNote/SearchList.java
+++ java/src/LeetCode/ListNote/SearchList.java
@@ -12,54 +12,40 @@ import java.util.List;
 */
 public class SearchList {
 
-    private int target;
     private int[] nums;
+    private int target;
 
-    public int search(int[] nums,int target){
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
         this.nums = nums;
         this.target = target;
-        int n = nums.length;
 
-        if(n==0) return -1;
-        if(n==1){
-            return this.nums[0]==target ? 0 : -1;
-        }
-        int midIndex = findMidIndex(0,n-1);
-        if(nums[midIndex] == target) return midIndex;
-        if(midIndex == 0) return search(0,n-1);
-        if(target<nums[0]) return search(midIndex, n-1);
-        return search(0,midIndex);
+        return recursion(0, nums.length - 1);
+
     }
 
-    private int findMidIndex(int start, int end) {
-        if(nums[start] < nums[end]) return 0;
-        while (start<=end){
-            int mid = (start + end)/2;
-            if(nums[mid] > nums[mid+1]) return mid+1;
-            else{
-                if(  nums[mid] < nums[start]) {
-                    end=mid-1;
-                }else{
-                    start=mid+1;
+    private int recursion(int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+//            前半段有序
+            else if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    return recursion(start, mid - 1);
+                } else {
+                    return recursion(mid + 1, end);
                 }
-            }
-
-        }
-        return 0;
-    }
-
-    private int search(int left,int right){
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid] == target) return mid;
-            else{
-                if(nums[mid] < target){
-                    left = mid+1;
-                }else {
-                    right = mid-1;
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    return recursion(mid + 1, end);
+                } else {
+                    return recursion(start, mid - 1);
                 }
+
             }
         }
         return -1;
     }
+
 }
+
